8d117db6a34b4e956e5d8f6d119c57aa15b25d1d
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-05-20 19:43:46 +0000

    added todo

--- src/main/java/de/cosmocode/palava/ipc/xml/rpc/adapters/Adapter.java
+++ src/main/java/de/cosmocode/palava/ipc/xml/rpc/adapters/Adapter.java
@@ -19,6 +19,8 @@ package de.cosmocode.palava.ipc.xml.rpc.adapters;
 /**
  * An adapter allows bidirectional conversion from one type to another.
  *
+ * TODO should be package private and accessed via an AdapterService
+ *
  * @since 1.0
  * @author Willi Schoenborn
  * @param <F> first type
